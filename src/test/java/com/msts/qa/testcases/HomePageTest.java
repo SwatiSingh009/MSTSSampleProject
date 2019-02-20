949b21e3ebffbc4022c27625363f2589d48447d7
SwatiSingh009 <47814673+SwatiSingh009@users.noreply.github.com>
2019-02-20 15:57:53 +0530

    Update HomePageTest.java

--- src/test/java/com/msts/qa/testcases/HomePageTest.java
+++ src/test/java/com/msts/qa/testcases/HomePageTest.java
@@ -16,11 +16,6 @@ package com.msts.qa.testcases;
 			super();
 		}
 
-		//test cases should be separated -- independent with each other
-		//before each test case -- launch the browser and login
-		//@test -- execute test case
-		//after each test case -- close the browser
-		
 		@BeforeMethod
 		public void setUp() {
 			initialization();
